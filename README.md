852ad3ebbfd69003e123d6a990e0152fcc96250a
SajeehaKS <47237143+SajeehaKS@users.noreply.github.com>
2022-09-13 23:09:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# small-website-jqpmks
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/small-website-jqpmks)
---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+
+import { ContactComponent } from './contact/contact.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { AcademicsComponent } from './academics/academics.component';
+const Routs: Routes = [
+  { path: 'home', component: HomeComponent },
+
+  { path: 'contact', component: ContactComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'academics', component: AcademicsComponent },
+];
+
+@NgModule({
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(Routs)],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HomeComponent,
+
+    ContactComponent,
+    IntroductionComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    AcademicsComponent,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
